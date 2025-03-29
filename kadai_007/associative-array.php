bcf3d4d5f94ac561725adde5d4f69f23826ed500
monta-tanke <ft380.ft100-ha100inclex.prefer@ezweb.ne.jp>
2025-03-29 14:35:21 +0900

    kadai_007

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<meta lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHPの基礎編</title>
+</head>
+
+<body>
+  <p>
+   <?php
+    $item_names=['name'=>'onion','price'=>'200','weight'=>'160',];
+
+    print_r($item_names);
+   ?>
+  </p>
+</body>
